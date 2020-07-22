08883c82fbaede274444fcc495ac466bfc2f54a6
Daksh Khetan <2012daksh@gmail.com>
2020-07-23 02:26:01 +0530

    update Workshops component

--- src/components/Workshops/Workshops.component.jsx
+++ src/components/Workshops/Workshops.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 
+import { ReactComponent as Image } from './workshopSVG.svg';
+
 import './Workshops.styles.scss';
 
 const Workshops = () => {
@@ -8,6 +10,17 @@ const Workshops = () => {
     <section id='workshops' className='section section-workshops'>
       <Fade bottom>
         <h1 className='section-heading'>Workshops</h1>
+        <div className='section-content'>
+          <div className='info'>
+            <p>
+              Workshops are high intensity sessions with more focus on building
+              projects. These are life changing as attendees build so much of
+              amazing stuff under the guidance of mentors.
+            </p>
+            <button>Check More Such Events</button>
+          </div>
+          <Image className='image-container' />
+        </div>
       </Fade>
     </section>
   );
