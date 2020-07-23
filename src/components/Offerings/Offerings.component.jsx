d25952d61d58ebe89a4402efd6594e2f011e24bb
Daksh Khetan <2012daksh@gmail.com>
2020-07-24 00:08:17 +0530

    update Offerings component & add icons in assets

--- src/components/Offerings/Offerings.component.jsx
+++ src/components/Offerings/Offerings.component.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 
+import WebIcon from '../../assets/technology/web-icon.png';
+import AndroidIcon from '../../assets/technology/android-icon.png';
+import iOSIcon from '../../assets/technology/ios-icon.png';
+import MLIcon from '../../assets/technology/ml-icon.png';
+import RoboticsIcon from '../../assets/technology/robotics-icon.png';
+import BlockchainIcon from '../../assets/technology/blockchain-icon.png';
+import { ReactComponent as Illustration } from '../../assets/illustrations/reading-book.svg';
+
 import './Offerings.styles.scss';
 
 const Offerings = () => {
   return (
     <section id='offerings' className='section section-offerings'>
       <Fade bottom>
-        <h1 className='section-heading'>Offerings</h1>
+        <h1 className='section-heading'>Latest Trends</h1>
+        <div className='sub-heading'>
+          <p>ACM takes all the latest technologies into consideration</p>
+        </div>
+        <div className='section-content'>
+          <div className='tech-container'>
+            <div className='image-container'>
+              <img src={WebIcon} alt='web-development' />
+            </div>
+            <div className='image-container'>
+              <img src={AndroidIcon} alt='android' />
+            </div>
+            <div className='image-container'>
+              <img src={iOSIcon} alt='ios' />
+            </div>
+            <div className='image-container'>
+              <img src={MLIcon} alt='machine-learning' />
+            </div>
+            <div className='image-container'>
+              <img src={RoboticsIcon} alt='robotics' />
+            </div>
+            <div className='image-container'>
+              <img src={BlockchainIcon} alt='blockchain' />
+            </div>
+          </div>
+          <div className='illustration-container'>
+            <div className='illustration'>
+              <Illustration />
+            </div>
+          </div>
+        </div>
       </Fade>
     </section>
   );
