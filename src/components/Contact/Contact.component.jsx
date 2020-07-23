3b11130e872ce04bd515b9cf32fece3d49428cab
Daksh Khetan <2012daksh@gmail.com>
2020-07-24 02:06:48 +0530

    add footer in contact page route

--- src/components/Contact/Contact.component.jsx
+++ src/components/Contact/Contact.component.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import { withRouter } from 'react-router-dom';
+
+import Footer from '../Footer/Footer.component';
 
 import { ReactComponent as Image } from '../../assets/illustrations/contact_img.svg';
-
 import './Contact.styles.scss';
 
-const Contact = () => {
+const Contact = ({ location }) => {
+  const addFooterCheck = () => {
+    return location.pathname === '/contact' && <Footer />;
+  };
+
   return (
-    <section id='contact' className='section section-contact'>
-      <Fade bottom>
-        <h1 className='section-heading'>Reach Us</h1>
-      </Fade>
-      <div className='section-content'>
-        <div className='info'>
-          <Fade bottom delay={200}>
-            <h2 className='heading'>ADDRESS</h2>
-          </Fade>
-          <Fade bottom delay={300}>
-            <p className='text'>A-4, Paschim Vihar, New Delhi - 110063</p>
-          </Fade>
-          <Fade bottom delay={400}>
-            <p className='text text-2'>
-              (In front of Paschim Vihar East Metro Station)
-            </p>
-          </Fade>
-          <Fade bottom delay={500}>
-            <div className='mail-btn'>
-              <a href='mailto:acmbvcoe@gmail.com'>Write To Us</a>
+    <React.Fragment>
+      <section id='contact' className='section section-contact'>
+        <Fade bottom>
+          <h1 className='section-heading'>Reach Us</h1>
+        </Fade>
+        <div className='section-content'>
+          <div className='info'>
+            <Fade bottom delay={200}>
+              <h2 className='heading'>ADDRESS</h2>
+            </Fade>
+            <Fade bottom delay={300}>
+              <p className='text'>A-4, Paschim Vihar, New Delhi - 110063</p>
+            </Fade>
+            <Fade bottom delay={400}>
+              <p className='text text-2'>
+                (In front of Paschim Vihar East Metro Station)
+              </p>
+            </Fade>
+            <Fade bottom delay={500}>
+              <div className='mail-btn'>
+                <a href='mailto:acmbvcoe@gmail.com'>Write To Us</a>
+              </div>
+            </Fade>
+          </div>
+          <Fade delay={100}>
+            <div className='image-container'>
+              <div className='illustration'>
+                <Image />
+              </div>
             </div>
           </Fade>
         </div>
-        <Fade delay={100}>
-          <div className='image-container'>
-            <div className='illustration'>
-              <Image />
-            </div>
-          </div>
-        </Fade>
-      </div>
-    </section>
+      </section>
+      {addFooterCheck()}
+    </React.Fragment>
   );
 };
 
-export default Contact;
+export default withRouter(Contact);
